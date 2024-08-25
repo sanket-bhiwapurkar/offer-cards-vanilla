433aaa7e9beccc7588f8e376f4af4fd7cecf058b
sanket-bhiwapurkar <sanket24bhiwapurkar@gmail.com>
2024-08-25 09:28:28 +0530

    class name changed | improved js

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-// selecting all the radio button input inside offer cards
-const offerCardsRadios = document.querySelectorAll(".offer-card .radio")
+function offerOptions() {
+    // selecting all the radio input inside offer cards
+    const offerCardsRadios = document.querySelectorAll(".offer-card .radio")
 
-// adding change event listener to each radio input
-offerCardsRadios.forEach(radio => {
-    radio.addEventListener('change', handleOfferCardRadioChange)
-})
+    console.log(offerCardsRadios)
+    // return if empty
+    if (offerCardsRadios.length === 0) return
 
-// defining the callback for radio input change event listener
-function handleOfferCardRadioChange(event) {
-    document.querySelectorAll('.offer-card').forEach(offerCard => {
-        // turning all the offer-card off when a radio input is changed and hiding all the relevant elements
-        offerCard.classList.remove("active")
-        offerCard.querySelectorAll('.active-show').forEach(item => item.style.display = "none")
+    // adding change event listener to each radio input
+    offerCardsRadios.forEach(radio => {
+        radio.addEventListener('change', handleOfferCardRadioChange)
     })
-    // slecting the label coontainer from inside which radio button was turned on
-    const offerCard = document.querySelector(`label[for="${event.target.id}"]`);
-    if (offerCard) {
-        // turning the offer-card on when the current radio input is Checked and showing all the relevant elements inside it
-        offerCard.classList.add('active');
-        offerCard.querySelectorAll('.active-show').forEach(item => item.style.display = "initial")
+
+    // defining radio input change event listener callback
+    function handleOfferCardRadioChange(event) {
+        document.querySelectorAll('.offer-card').forEach(offerCard => {
+            // collapsing all offer card
+            offerCard.classList.remove("expanded")
+            // Hiding relevant elements when collapsed
+            offerCard.querySelectorAll('.expanded-show').forEach(item => item.style.display = "none")
+        })
+        // slecting the label coontainer from inside which radio button was turned on
+        const offerCard = document.querySelector(`label[for="${event.target.id}"]`);
+        if (offerCard) {
+            // expanding all offer card
+            offerCard.classList.add('expanded');
+            // Hiding relevant elements when expanded
+            offerCard.querySelectorAll('.expanded-show').forEach(item => item.style.display = "initial")
+        }
     }
 }
+
+offerOptions()
